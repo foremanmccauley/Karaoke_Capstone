da369c805f7bdde641f38805c1e9c1d685370c4d
jmet <jmet98@att.net>
2021-03-17 15:06:35 -0500

    add form for sign up page and render it on page

--- karaoke/karaoke/views.py
+++ karaoke/karaoke/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 
 def index(request):
     return render(request, 'index.html')
+
 def recording(request):
     return render(request, 'recording.html')
+
 def songselection(request):
     return render(request, 'songselection.html')
+
 def register(request):
-    return render(request, 'register.html')
+    form = RegistrationForm()
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
 def login(request):
     return render(request, 'login.html')
