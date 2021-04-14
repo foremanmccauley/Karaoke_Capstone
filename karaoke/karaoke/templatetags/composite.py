63c119f27d5d3b407e5c264926ec5a19e87bb0fa
Michaela Matocha <mich.a.matocha@tamu.edu>
2021-04-14 17:11:22 -0500

    AUDIO OVERLAY!!!!

--- karaoke/karaoke/templatetags/composite.py
+++ karaoke/karaoke/templatetags/composite.py
@@ -2,6 +2,7 @@ import requests
 from django import template
 from django.utils.safestring import mark_safe
 from moviepy.editor import VideoFileClip, AudioFileClip, CompositeAudioClip, clips_array
+from moviepy.audio.fx.volumex import volumex
 
 register = template.Library()
 
@@ -27,6 +28,11 @@ def comp(request):
     mp3 = AudioFileClip('karaoke/static/media/rr.mp3')
     flist = ['karaoke/static/media/testfiles/1.mp4', 'karaoke/static/media/testfiles/2.mov', 'karaoke/static/media/testfiles/4.mov', 'karaoke/static/media/testfiles/5.mov']
 
+#test clip arrays
+# space: ['karaoke/static/media/testfiles/1.mp4', 'karaoke/static/media/testfiles/2.mov', 'karaoke/static/media/testfiles/4.mov', 'karaoke/static/media/testfiles/5.mov']
+# ryan: ['ryan1.mov', 'ryan2.mov']
+
+def comp(request, uid='001', mp3=AudioFileClip('karaoke/static/media/testfiles/rr.mp3'), flist=['karaoke/static/media/testfiles/ryan1.mov', 'karaoke/static/media/testfiles/ryan2.mov']):
     arr = []
     cols = 2
     temp = []
@@ -36,11 +42,12 @@ def comp(request):
         if i % cols == 0 and i != 0:
             arr.append(temp)
             temp = []
-        temp.append(VideoFileClip(flist[i]))
+        temp.append(VideoFileClip(flist[i]).margin(10))
     arr.append(temp)
 
     final = clips_array(arr)
+    mp3 = mp3.fx(volumex, 0.25)
     audio = CompositeAudioClip([mp3, final.audio])
-
-    ret = final.write_videofile("test.mp4", audio=audio)
+    final = final.set_audio(audio)
+    final.write_videofile("karaoke/static/media/outputs/" + uid + ".mp4", codec='libx264', audio_codec='aac')
     return(final)
